ec4f87c3df9710817c4ee43cc1ea720934fd0371
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2023-12-25 16:19:24 +0200

    Removed unNecessary Dependencies

--- OrderingSystem/src/main/java/OrderingSystem/Shipping/Entities/IShippingCalculator.java
+++ OrderingSystem/src/main/java/OrderingSystem/Shipping/Entities/IShippingCalculator.java
@@ -1,7 +1,5 @@
 package OrderingSystem.Shipping.Entities;
 
-import OrderingSystem.Address.Address;
-import OrderingSystem.Orders.Entities.IOrderComponent;
 import OrderingSystem.Orders.Entities.SimpleOrder;
 
 public interface IShippingCalculator {
